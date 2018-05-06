59334543887f960f121ae95546c60d3888585924
achicha <meth787@gmail.com>
2018-05-06 13:45:32 +0300

    feat: add ajax product's edit in basket

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 from basketapp.models import Basket
 from products.models import Product
@@ -47,3 +49,28 @@ def basket_remove(request, pk):
     basket_record.delete()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, pk, quantity):
+    if request.is_ajax():
+        quantity = int(quantity)
+        new_basket_item = Basket.objects.get(pk=int(pk))
+
+        if quantity > 0:
+            new_basket_item.quantity = quantity
+            new_basket_item.save()
+        else:
+            new_basket_item.delete()
+
+        basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
+
+        content = {
+            'basket_items': basket_items,
+        }
+
+        result = render_to_string('basket_list.html', content)
+
+        return JsonResponse({'result': result})
+    else:
+        return HttpResponseRedirect(reverse('basket:view'))
